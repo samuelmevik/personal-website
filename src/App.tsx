b0686cf81d4a36f4bfb2392f9a5a7a942205bcc5
samuelmevik <samuel.mevik@outlook.com>
2024-09-02 22:01:49 +0200

    Misc: reveal to hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,46 +2,20 @@ import Education from './components/Education'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import WorkExperience from './components/WorkExperience'
-import { useEffect } from 'react'
+import { useRef } from 'react'
 import SnippetsFromMyLife from './components/SnippetsFromMyLife'
 import ThemeToggler from './components/ThemeToggler'
 import Projects from './components/Projects'
 import AboutMe from './components/AboutMe'
 import Menu from './components/Menu'
+import useRevealOnScroll from './hooks/RevealOnScroll'
 
-
-
-
-
-// <Profile className='py-4 sm:py-12' />
 function App() {
-
-  useEffect(() => {
-    function animate() {
-      const reveals = document.querySelectorAll('.reveal')
-      for (let i = 0; i < reveals.length; i++) {
-        const windowHeight = window.innerHeight;
-        const elementTop = reveals[i].getBoundingClientRect().top;
-        const elementVisible = 100;
-
-        if (elementTop < windowHeight - elementVisible) {
-          reveals[i].classList.add("active");
-        } else {
-          reveals[i].classList.remove("active");
-        }
-      }
-    }
-
-    window.addEventListener("scroll", animate, { passive: true })
-
-    return () => {
-      window.removeEventListener("scroll", animate);
-    };
-
-  })
+  const ref = useRef<HTMLDivElement>(null)
+  useRevealOnScroll(ref)
 
   return (
-    <div className='overflow-hidden bg-white transition-colors duration-500 dark:bg-black dark:text-white'>
+    <div ref={ref} className='overflow-hidden bg-white transition-colors duration-500 dark:bg-black dark:text-white'>
       <ThemeToggler />
       <Menu />
       <Header />
@@ -51,7 +25,6 @@ function App() {
       <Education />
       <Projects />
       <Footer />
-
     </div>
   )
 }
