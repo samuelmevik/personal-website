3ebcc77b0a1cd824ba97e3ad6a51ba20a4cea2e2
samuelmevik <samuel.mevik@outlook.com>
2024-09-02 13:46:34 +0200

    Add: Gap for clarity

--- src/components/ui/Experience.tsx
+++ src/components/ui/Experience.tsx
@@ -1,7 +1,7 @@
 
 function Experience({ children, title, date, place }: { children: React.ReactNode, title: string, date?: string, place?: string }) {
   return (
-    <div className="flex flex-col p-1">
+    <div className="flex flex-col p-1 gap-4">
       <div className="flex justify-between">
         <p className="font-semibold">{title}</p>
         {date && <p className="self-center" style={{fontSize: "clamp(0.5rem, 1vw, 1rem)"}}>{date}</p>}
