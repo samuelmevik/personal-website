7df034846f0769f29c0c51a5dd4cb27accbc8333
samuelmevik <samuel.mevik@outlook.com>
2024-09-14 10:44:14 +0200

    Misc: Better resizing

--- src/components/ui/Grid.tsx
+++ src/components/ui/Grid.tsx
@@ -6,7 +6,7 @@ function Grid({ children, className }: { children: React.ReactNode, className?: 
 
 
 Grid.Item = function ({ children, className }: { children?: React.ReactNode, className?: string }) {
-  return <div className={twMerge("w-[90%] md:w-[55vmin] duration-700 mx-auto transition-[width,height,transform] ease-in-out md:even:translate-x-[calc(33%+2vw)] md:odd:-translate-x-[calc(33%+2vw)]", className)}>{children}</div>
+  return <div className={twMerge("w-[90%] md:w-[max(55vmin,30rem)] duration-700 mx-auto transition-[width,height,transform] ease-in-out md:even:translate-x-[calc(33%+2vw)] md:odd:-translate-x-[calc(33%+2vw)]", className)}>{children}</div>
 }
 
 export default Grid
