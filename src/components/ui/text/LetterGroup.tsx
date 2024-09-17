381dce75f2054aea843cd3cfe3e7f4373615645a
samuelmevik <samuel.mevik@outlook.com>
2024-09-17 17:06:01 +0200

    Misc: Hover animation imp

--- src/components/ui/text/LetterGroup.tsx
+++ src/components/ui/text/LetterGroup.tsx
@@ -2,7 +2,7 @@ import { twMerge } from "tailwind-merge";
 
 function LetterGroup({ children, className }: { children: React.ReactNode, className?: string }) {
   return (
-    <div className={twMerge("group/sentence group-hover:opacity-10 hover:!opacity-100 *:inline-block *:transition-transform", className)} >
+    <div className={twMerge("group/sentence transition-opacity duration-500 ease-in-out group-hover:opacity-10 hover:!opacity-100 *:inline-block *:transition-transform *:duration-500 *:ease-in-out", className)} >
       {children}
     </div>
   );
--- src/components/ui/text/SentenceGroup.tsx
+++ src/components/ui/text/SentenceGroup.tsx
@@ -2,7 +2,7 @@ import { twMerge } from "tailwind-merge";
 
 function SentenceGroup({ children, className, props }: { children: React.ReactNode, className?: string, props? : React.HTMLAttributes<HTMLDivElement> }) {
   return (
-    <h1 className={twMerge("group *:transition-opacity", className)} {...props}>
+    <h1 className={twMerge("group", className)} {...props}>
       {children}
     </h1>
   );
