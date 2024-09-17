b38e9a59e60f400da64c58400a4e071c42c27943
samuelmevik <samuel.mevik@outlook.com>
2024-09-17 17:04:20 +0200

    Misc: Removed padding

--- src/components/TitleWrapper.tsx
+++ src/components/TitleWrapper.tsx
@@ -1,7 +1,7 @@
 
 
 function TitleWrapper({ children } : { children: React.ReactNode }) {
-  return <div className="text-center py-[clamp(1rem,5vh,3rem)]">{children}</div>;
+  return <div className="text-center">{children}</div>;
 }
 
 export default TitleWrapper;
