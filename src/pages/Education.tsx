ee1643816f4a8cd2d5fc6b5e7bd9fc67fa1dcbd9
samuelmevik <samuel.mevik@outlook.com>
2024-09-17 17:04:38 +0200

    Misc: Decreased img size

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable tailwindcss/classnames-order */
 import PageWrapper from "../components/PageWrapper"
 import TitleWrapper from "../components/TitleWrapper"
 import Experience from "../components/ui/Experience"
 import Grid from "../components/ui/Grid"
 import LetterGroup from "../components/ui/text/LetterGroup"
 import SentenceGroup from "../components/ui/text/SentenceGroup"
+import lnuSmall from "../assets/lnu-S.jpg"
+import lnuLarge from "../assets/lnu-L.jpg"
 
 
 function Education() {
@@ -13,26 +16,30 @@ function Education() {
         <SentenceGroup className="inline-flex w-fit flex-wrap">
           <LetterGroup>
             <span className="group-hover/sentence:translate-x-[-80%] group-hover/sentence/sentence:translate-y-[60%] group-hover/sentence/sentence:rotate-[4deg]">M</span>
-            <span className="group-hover/sentence:translate-x-[-40%] group-hover/sentence:translate-y-[20%] group-hover/sentence:rotate-[-6deg] transform">y</span>
+            <span className="group-hover/sentence:translate-x-[-40%] group-hover/sentence:translate-y-[20%] group-hover/sentence:-rotate-6">y</span>
             <span>&nbsp;</span>
           </LetterGroup>
           <LetterGroup>
-            <span className="group-hover/sentence:translate-x-[-10%] group-hover/sentence:translate-y-[60%] group-hover/sentence:rotate-[-8deg] transform">E</span>
-            <span className="group-hover/sentence:translate-y-[8%] group-hover/sentence:rotate-[5deg] transform">d</span>
-            <span className="group-hover/sentence:translate-y-[20%] group-hover/sentence:rotate-[-3deg] transform">u</span>
-            <span className="group-hover/sentence:translate-y-[-40%] group-hover/sentence:rotate-[-5deg] transform">c</span>
-            <span className="group-hover/sentence:translate-y-[-20%] group-hover/sentence:rotate-[-5deg] transform">a</span>
-            <span className="group-hover/sentence:translate-y-[-5%] group-hover/sentence:rotate-[-5deg] transform">t</span>
-            <span className="group-hover/sentence:translate-y-[40%] group-hover/sentence:rotate-[-5deg] transform">i</span>
-            <span className="group-hover/sentence:translate-y-[-20%] group-hover/sentence:rotate-[-5deg] transform">o</span>
-            <span className="group-hover/sentence:translate-y-[-5%] group-hover/sentence:rotate-[-5deg] transform">n</span>
+            <span className="group-hover/sentence:translate-x-[-10%] group-hover/sentence:translate-y-[60%] group-hover/sentence:rotate-[-8deg]">E</span>
+            <span className="group-hover/sentence:translate-y-[8%] group-hover/sentence:rotate-[5deg]">d</span>
+            <span className="group-hover/sentence:translate-y-[20%] group-hover/sentence:-rotate-3">u</span>
+            <span className="group-hover/sentence:translate-y-[-40%] group-hover/sentence:rotate-[-5deg]">c</span>
+            <span className="group-hover/sentence:translate-y-[-20%] group-hover/sentence:rotate-[-5deg]">a</span>
+            <span className="group-hover/sentence:translate-y-[-5%] group-hover/sentence:rotate-[-5deg]">t</span>
+            <span className="group-hover/sentence:translate-y-[40%] group-hover/sentence:rotate-[-5deg]">i</span>
+            <span className="group-hover/sentence:translate-y-[-20%] group-hover/sentence:rotate-[-5deg]">o</span>
+            <span className="group-hover/sentence:translate-y-[-5%] group-hover/sentence:rotate-[-5deg]">n</span>
           </LetterGroup>
         </SentenceGroup>
       </TitleWrapper>
 
       <Grid className="flex-1 place-items-center">
-        <div className="md:max-w-[max(80vmin,40rem)] w-[90%]">
-          <img width={412} height={147} className="object-cover w-full" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.timeshighereducation.com%2Fsites%2Fdefault%2Ffiles%2Finstitution%2Fheader_image%2Flnu-header-image-1950x700.jpg&f=1&nofb=1&ipt=3826ebcc7505c0532ce5fc8d33589c12f6b5e7901a072923a0286d397093d213&ipo=images" alt="Linneaus University" />
+        <div className="w-[90%] md:max-w-[max(80vmin,40rem)]">
+          <picture>
+            <source width={640} height={230} media="(max-width: 768px)" srcSet={lnuSmall} />
+            <source width={900} height={323} media="(min-width: 768px)" srcSet={lnuLarge} />
+            <img width={900} height={323} className="object-cover w-full" src={lnuLarge} alt="Linnaeus University" />
+          </picture>
           <Experience title="Bachelor of Web Development" date="August 2021 - June 2024" place="Linnaeus university">
             <p>My education has provided me with a solid computer science foundation, focusing
               on web programming.
